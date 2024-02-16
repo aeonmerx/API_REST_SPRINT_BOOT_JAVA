4ac191c824e15af1018bf1b942c094c7f6ddb880
aeonmerx <aeon.merx@gmail.com>
2024-02-16 18:53:20 -0500

    Mi primer commit

---
+++ src/test/java/com/aeonmerx/proyectopeguirest/ProyectoPeguiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeonmerx.proyectopeguirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPeguiRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
